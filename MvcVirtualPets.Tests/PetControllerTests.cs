5ce77d19a6f8a2f67bd147771dc8de4dceaf76b7
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-03-06 12:16:01 -0500

    Introduced NSubstitute

--- MvcVirtualPets.Tests/PetControllerTests.cs
+++ MvcVirtualPets.Tests/PetControllerTests.cs
@@ -1,44 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using MvcVirtualPets.Controllers;
 using MvcVirtualPets.Models;
+using MvcVirtualPets.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using NSubstitute;
 
 namespace MvcVirtualPets.Tests
 {
     public class PetControllerTests
     {
         PetController underTest;
+        private IPetRepository repo;
+
         public PetControllerTests()
-        {
-            underTest = new PetController();
+        {            
+            repo = Substitute.For<IPetRepository>();
+            underTest = new PetController(repo);
         }
 
         [Fact]
         public void Index_Returns_A_ViewResult()
-        {
-            var underTest = new PetController();
+        {            
             var result = underTest.Index();
             Assert.IsType<ViewResult>(result);
         }
 
         [Fact]
-        public void Index_Model_Has_3_Pets()
+        public void Index_Model_Is_Expected_Model()
         {
+            var expectedModel = new List<Pet>();
+            repo.GetAll().Returns(expectedModel);
+
             var result = underTest.Index();
             var model = (IEnumerable<Pet>)result.Model;
-            Assert.Equal(3, model.Count());
+            Assert.Equal(expectedModel, model);
         }
-
         [Fact]
-        public void Details_Model_Has_Correct_Id()
+        public void Details_Model_Is_Expected_Model()
         {
             var expectedId = 2;
+            var expectedModel = new Pet();
+            repo.GetById(expectedId).Returns(expectedModel);
+         
             var result = underTest.Details(expectedId);
             var model = (Pet)result.Model;
-            Assert.Equal(expectedId, model.Id);
+
+            Assert.Equal(expectedModel, model);
         }
     }
 }
--- MvcVirtualPets/Startup.cs
+++ MvcVirtualPets/Startup.cs
@@ -20,7 +20,7 @@ namespace MvcVirtualPets
 
             services.AddDbContext<PetContext>();
 
-            services.AddScoped<PetRepository, PetRepository>();
+            services.AddScoped<IPetRepository, PetRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
